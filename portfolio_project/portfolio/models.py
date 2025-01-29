8feceac7dd4f0dd4e75a3a3970dd8d74f763585f
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-01-29 06:45:02 -0800

    update

--- portfolio_project/portfolio/models.py
+++ portfolio_project/portfolio/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField()
-    link = models.URLField
+    image = models.ImageField(upload_to='project_images/', blank=True, null=True)
+    link = models.URLField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
--- portfolio_project/portfolio/views.py
+++ portfolio_project/portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def index(request):
-    return render(request, 'portfolio/index.html') # Renders the index.html template
+    projects = Project.objects.all() # Get all prpjects from database
+    return render(request, 'portfolio/index.html', {'projects': projects}) # Renders the index.html template and pass the projects to the template
