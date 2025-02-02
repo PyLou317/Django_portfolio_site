6a9610024bb3512160ff1f0dc94629bcc3897e6c
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-02-01 19:02:54 -0800

    fix broken url

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls.static import static
 app_name = 'portfolio' # needed for namespacing
 
 urlpatterns = [
-    path('', views.home, name='home'), # Maps the root URL to the index vie
-    path('projects/<int:pk>/', views.project_detail, name='project_detail'),
+    path('', views.home, name='home'), # Maps the root URL to the index view
+    path('projects/<int:pk>/', views.project_detail, name='portfolio:project_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio/views.py
+++ portfolio/views.py
@@ -6,10 +6,8 @@ def home(request):
     projects = Project.objects.all() # Get all prpjects from database
     return render(request, 'portfolio/index.html', {'projects': projects}) # Renders the index.html template and pass the projects to the template
 
+
 def project_detail(request, pk):
-    project = get_object_or_404(Project, pk=pk) # Uses Project and id to return project details, return 404 if not found
-    return render(
-        request,
-        "portfolio/project_detail.html",
-        {"project": project} # Turns project object into dict for rendering on the .html page
-    )
+    project = get_object_or_404(Project, pk=pk)
+    context = {'project': project}  # Pass the project object to the context
+    return render(request, 'project_detail.html', context)
