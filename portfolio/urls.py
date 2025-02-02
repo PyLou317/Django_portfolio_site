389799399fdf826471b89645a925204fdb062bae
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-02-02 06:26:21 -0800

    change card css

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -8,5 +8,5 @@ app_name = 'portfolio' # needed for namespacing
 
 urlpatterns = [
     path('', views.home, name='home'), # Maps the root URL to the index view
-    path('projects/<int:pk>/', views.project_detail, name='portfolio:project_detail'),
+    path('projects/<int:pk>/', views.project_detail, name='project_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
