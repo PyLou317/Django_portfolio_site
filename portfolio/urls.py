879b4482172f2ea08c29e43ec9d1301c5a487c6e
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-02-03 05:24:36 -0800

    changed comment on portfolio url

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls.static import static
 app_name = 'portfolio' # needed for namespacing
 
 urlpatterns = [
-    path('', views.home, name='home'), # Maps the root URL to the index view
+    path('', views.home, name='home'), # Maps the root URL to the index view in the portfolio app
     path('projects/<int:pk>/', views.project_detail, name='project_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
