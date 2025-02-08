f40ca5d8d7e45f70c942f0cd4ef6d556be04c6f0
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-02-08 15:54:34 -0800

    added pagination

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Project(models.Model):
@@ -6,6 +7,9 @@ class Project(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='project_images/', blank=True, null=True)
     link = models.URLField(blank=True, null=True)
+    
+    def get_absolute_url(self):
+        return reverse('project_detail', args=[str(self.id)]) 
     
     def __str__(self):
         return self.title
