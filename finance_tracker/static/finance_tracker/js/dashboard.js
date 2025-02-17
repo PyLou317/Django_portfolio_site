eff54575d69eeb2047c27a01b043dc973c59fda5
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-02-17 13:33:10 -0800

    updated charts

--- finance_tracker/static/finance_tracker/js/dashboard.js
+++ finance_tracker/static/finance_tracker/js/dashboard.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const totalExpensesChart = document.getElementById('total-expenses-chart')
     const monthlyExpensesChart = document.getElementById('monthly-expenses-chart')
     const totalIncomeChart = document.getElementById('total-income-chart')
+    const expenseByMonthChart = document.getElementById('expense-by-month-chart')
 
     
     new Chart(totalExpensesChart, {
@@ -21,5 +22,43 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             }
         }
-    })
-})   
+    });
+
+    const monthlyExpense = new Chart(monthlyExpensesChart, {
+        type: 'line',
+        data: {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            datasets: [{
+                label: 'Expenses',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    const expenseByMonth = new Chart(expenseByMonthChart, {
+        type: 'bar',
+        data: {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            datasets: [{
+                label: 'Expenses',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+})
