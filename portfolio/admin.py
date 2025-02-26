2ae242a4562c55b3d90a111dd9b224474be23765
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-02-26 06:07:26 -0800

    fixed project card spacing and added tech icons

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Project, Technology
 
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    filter_horizontal = ('technologies',)
+    
+    
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Technology)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+
+class Technology(models.Model):
+    name = models.CharField(max_length=100, unique=True, help_text="Name of the technology (e.g., Python, Django)")
+    icon = models.ImageField(upload_to='technology_icons/', blank=True, null=True)
+    
+    class Meta:
+        verbose_name_plural = "Technologies"
+    
+    def __str__(self):
+        return self.name
+    
+    
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='project_images/', blank=True, null=True)
     github = models.URLField(blank=True, null=True)
+    technologies = models.ManyToManyField(Technology, blank=True, help_text="Technology used in this project")
     TYPES = (
         ('FINANCE', 'Fiinance Tracker'),
         ('BLOG', 'Personal Blog'),
@@ -24,12 +36,3 @@ class Project(models.Model):
     def __str__(self):
         return self.title
 
-class Technology(models.Model):
-    name = models.CharField(max_length=100, unique=True, help_text="Name of the technology (e.g., Python, Django)")
-    icon = models.ImageField(upload_to='technology_icons/', blank=True, null=True)
-    
-    class Meta:
-        verbose_name_plural = "Technologies"
-    
-    def __str__(self):
-        return self.name
