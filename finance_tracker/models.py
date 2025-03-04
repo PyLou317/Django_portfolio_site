f45f26009669b526c7a4886d8633dbe9b5e3e02e
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-03-04 06:51:30 -0800

    reset database to add owner field to transaction model

--- finance_tracker/models.py
+++ finance_tracker/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Category(models.Model):
@@ -15,6 +16,7 @@ class Transaction(models.Model):
     description = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.date} - {self.description} - ${self.amount} - {self.category}"
