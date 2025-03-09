72403864a575ee23076da1956aca3d807205aacf
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-03-09 07:14:21 -0700

    added user avatar to user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/users/user_<id>/<filename>
+    return "user_{0}/{1}".format(instance.user.id, filename)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='users/{user_directory_path}/', blank=True, null=True)
+    
+    def __str__(self):
+        return f'{User.first_name} {User.last_name}'
