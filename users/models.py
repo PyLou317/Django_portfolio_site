23fc9a463bc315ae09c9d31e9784482b27be5e4e
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-03-09 15:26:05 -0700

    add user auth to side bar for uder links

--- users/models.py
+++ users/models.py
@@ -1,14 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/users/user_<id>/<filename>
     return "user_{0}/{1}".format(instance.user.id, filename)
-
+    
+    
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     avatar = models.ImageField(upload_to='users/{user_directory_path}/', blank=True, null=True)
     
     def __str__(self):
-        return f'{User.first_name} {User.last_name}'
+        return f'Profile for {self.user.username}'
+    
+    
+# Save instance of UserProfile when new user is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created: # Check if it's a new User being created
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- users/views.py
+++ users/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def user_profile(request):
     user = request.user
+    user_profile = request.user.profile
     context = {
-        'user': user
+        'user': user,
+        'user_profile': user_profile
     }
     return render(request, 'users/profile.html', context)
