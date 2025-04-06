400f733f0ba211e4fc447fcfe6e8bc95cb48a301
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-04-05 23:26:43 -0700

    styled cat card for 3 columns

--- finance_tracker/static/finance_tracker/js/category_list.js
+++ finance_tracker/static/finance_tracker/js/category_list.js
@@ -1,35 +1,34 @@
-"use strict";
+'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+  fetch('/finance_tracker/category_expense_json/')
+    .then((resp) => resp.json())
+    .then((categoryData) => {
+      console.log('Fetched data:', categoryData);
+      categoryData.forEach((data) => {
+        let expTotalRaw = parseFloat(data.total_expense);
+        let expTotal = isNaN(expTotalRaw) ? 'Error' : expTotalRaw.toFixed(2);
 
-    fetch('/finance_tracker/category_expense_json/')
-        .then(resp => resp.json())
-        .then(categoryData => {
-            console.log('Fetched data:', categoryData);
-            categoryData.forEach(data => {
-                let expTotalRaw = parseFloat(data.total_expense);
-                let expTotal = isNaN(expTotalRaw) ? 'Error' : expTotalRaw.toFixed(2);
-                
-                // Format the number with commas
-                const formattedExpTotal = "$ " + Number(expTotal).toLocaleString('en-CA', {
-                    minimumFractionDigits: 2,
-                    maximumFractionDigits: 2
-                });
+        // Format the number with commas
+        const formattedExpTotal =
+          '$ ' +
+          Number(expTotal).toLocaleString('en-CA', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          });
 
-                let extraInfo = '';
-                if (expTotalRaw < -5000) {
-                    extraInfo = '<span class="text-warning ms-1">(High Expense)</span>';
-                };
+        let extraInfo = '';
+        if (expTotalRaw < -5000) {
+          extraInfo = '<span class="text-warning ms-1">(High Expense)</span>';
+        }
 
-                const markup = `
+        const markup =`
                 <div class="card rounded-3 border-0 bg-light shadow-sm">
                     <div class="card-body p-3">
                         <div class="d-flex align-items-center justify-content-between mb-2">
                         <div class="d-flex align-items-center">
                             <div class="bg-success text-white rounded-circle d-flex align-items-center justify-content-center me-2" style="width: 30px; height: 30px;">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wallet" viewBox="0 0 16 16">
-                                <path d="M0 3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V3zm0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V7zm0 4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1v-2z"/>
-                            </svg>
+                                <i class="bi bi-cash-coin"></i>
                             </div>
                             <div>
                             <h6 class="mb-0">${data.category}</h6>
@@ -38,9 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
                             </div>
                         </div>
                         <div class="ms-auto">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-                            </svg>
+                            <i class="bi bi-chevron-right"></i>
                         </div>
                         </div>
                         <div class="d-flex justify-content-between align-items-center">
@@ -51,15 +48,16 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
                 `;
 
-                document.querySelector('#category-card').insertAdjacentHTML('beforeend', markup)
-            })
-        
-            
-        })
-        .catch(error => console.log(error));
-})
+        document
+          .querySelector('#category-card')
+          .insertAdjacentHTML('beforeend', markup);
+      });
+    })
+    .catch((error) => console.log(error));
+});
 
-{/* <div class="col-sm-12 col-md-6 mb-2">
+{
+  /* <div class="col-sm-12 col-md-6 mb-2">
     <div class="card text-start">
         <div class="card-body bg-light p-2">
             <div class="row d-flex align-items-center justify-content-between">
@@ -73,6 +71,5 @@ document.addEventListener('DOMContentLoaded', () => {
             </div>
         </div>
     </div>
-</div>   */}
-
-                
+</div>   */
+}
